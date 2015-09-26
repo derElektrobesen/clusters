04f8c082690305b956ba654d462e1243ccd9a5f4
Pavel Berezhnoy <pberezhnoy2005@gmail.com>
2015-09-26 11:22:02 +0300

    logs moved

--- gen_job.sh
+++ gen_job.sh
@@ -6,5 +6,6 @@ NODES_COUNT="$3"
 
 JOB_FILE=small.job
 
+mkdir -p $INITIAL_DIR/logs
 perl -pe "s#___APP_NAME___#$APP_NAME#g;s#___INITIAL_DIR___#$INITIAL_DIR#g;s#___NODES_COUNT___#$NODES_COUNT#g" \
     $INITIAL_DIR/../$JOB_FILE > $INITIAL_DIR/../$APP_NAME.job
