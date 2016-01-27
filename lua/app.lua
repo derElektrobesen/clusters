971f38463589fd6c307ecdeb733de89f499dde84
Pavel Berezhnoy <pberejnoy2005@gmail.com>
2016-01-27 12:43:09 +0300

    Tuple finding implementd

--- lua/app.lua
+++ lua/app.lua
@@ -33,6 +33,7 @@ box.once("space_creation", function ()
 
 	-- Space contains a list of tuples of type specified.
 	--	{ 'id', 'type_id', ... }
+	-- XXX: set tuples_data_offset if scheme will be changed
 	s = box.schema.space.create('tuples')
 	s:create_index('required_unique', {
 		type = 'tree',
@@ -48,6 +49,9 @@ box.once("space_creation", function ()
 		},
 	})
 end)
+
+local tuples_data_offset = 2
+local Null = {} -- needed to store nil values into table
 
 local json = require("json")
 local log = require("log")
@@ -66,7 +70,7 @@ tuple_space = {
 				-- Tuple type found. Store a tuple type and tuple size
 				cur_type = { v[2], v[3] }
 				if #v > 3 then
-					cur_val = { select(4, unpack(v)) }
+					cur_val = v[4] -- tuple values
 				end
 			elseif v[1] == 'TYPE' then
 				-- Trying to select any value of specified type
@@ -78,7 +82,7 @@ tuple_space = {
 			end
 
 			table.insert(types, cur_type)
-			table.insert(values, cur_val)
+			table.insert(values, cur_val or Null)
 		end
 
 		return types, values
@@ -87,6 +91,8 @@ tuple_space = {
 	__compare_tuple_types = function (expected_types, found_types)
 		local max_index = math.max(#expected_types, #found_types)
 		local equal = false
+
+		log.info("Comparing types " .. json.encode(expected_types) .. " and " .. json.encode(found_types))
 
 		for i, x in ipairs(expected_types) do
 			local y = found_types[i]
@@ -97,7 +103,7 @@ tuple_space = {
 
 			if type(x) == 'table' and not tuple_space.__compare_tuple_types(x, y) then
 				break
-			elseif x ~= y then
+			elseif type(x) ~= 'table' and x ~= y then
 				break
 			end
 
@@ -113,9 +119,11 @@ tuple_space = {
 	end,
 
 	__find_type = function (types_found)
+		log.info("__find_type")
 		-- Function searchs a tuple type same with a type of given tuple
 		-- Returns an index of this type or nil if not found
 		for _, tuple in box.space.types.index.primary:pairs(nil, { iterator = box.index.ALL }) do
+			log.info(json.encode({ tuple:unpack() }))
 			-- remove first tuple element(index) and compare types
 			if tuple_space.__compare_tuple_types({ select(2, tuple:unpack()) }, types_found) then
 				return tuple[1]
@@ -134,9 +142,52 @@ tuple_space = {
 
 	get_tuple = function (...)
 		-- Get tuple from tuple space, remove it from tuple space and return to client
-		-- TODO xxx
-		log.info("Tuple came into get_tuple: " .. json.encode({...}))
-		return {}
+		local types, values = tuple_space.__tuple_to_stuff({...})
+		local type_id = tuple_space.__find_type(types)
+
+		if type_id == nil then
+			log.info("Tuple with type " .. json.encode({...}) .. " not found in tuple space")
+			return nil
+		end
+
+		log.info("Tuple came into get_tuple: " .. json.encode({...}) .. ", type_id is " .. type_id)
+			log.info(json.encode(values))
+
+		for _, t in box.space.tuples.index.primary:pairs({ typeid }, { iterator = box.index.EQ }) do
+			local tuple_ok = true
+			for i, v in ipairs(values) do
+				log.info(v)
+				if v ~= Null then
+					-- Types are already equal
+					local tuple_data = t[tuples_data_offset + i]
+					if type(v) == 'table' then
+						log.info("---> " .. json.encode(tuple_data) .. " " .. json.encode(v))
+						if #v > #tuple_data then
+							tuple_ok = false
+							break
+						end
+
+						for i, tv in ipairs(v) do
+							if tv ~= tuple_data[i] then
+								tuple_ok = false
+								break
+							end
+						end
+					elseif tuple_data ~= v then
+						tuple_ok = false
+						break
+					end
+				end
+			end
+
+			if tuple_ok then
+				local tuple = { select(tuples_data_offset + 1, t:unpack()) }
+				log.info("Tuple found with required type: " .. json.encode({...}))
+				return tuple
+			end
+		end
+
+		return nil
 	end,
 
 	add_tuple = function (...)
